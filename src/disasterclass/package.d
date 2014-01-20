be0b5e566fe71c7a71fd0f0516daba94d936cda8
Simon Harris <simon@pearfalse.co.uk>
2014-01-20 22:40:07 +0000

    {A} package.d (see http://dlang.org/changelog.html#import_package for more details)

---
+++ src/disasterclass/package.d
@@ -0,0 +1,14 @@
+module disasterclass;
+
+public import disasterclass.versioninfo;
+public import disasterclass.support;
+public import disasterclass.vectors;
+public import disasterclass.cbuffer;
+public import disasterclass.nbt;
+public import disasterclass.region;
+public import disasterclass.chunk;
+public import disasterclass.world;
+public import disasterclass.data;
+public import disasterclass.biomedata;
+public import disasterclass.parallel;
+public import disasterclass.validate;
